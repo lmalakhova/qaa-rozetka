2ea22a0ac754410c664a1989906e2d15cd56ddc3
lmalakhova <lmalakhova0307@gmail.com>
2018-06-03 11:38:29 +0300

    Added the following: - specify Test Class with Javadoc; - add Try.run() for InterruptedException handling.

--- src/test/java/io/github/lmalakhova/RozetkaTests.java
+++ src/test/java/io/github/lmalakhova/RozetkaTests.java
@@ -1,22 +1,27 @@
 package io.github.lmalakhova;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.vavr.control.Try;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+/**
+ * Tests for Rozetka.com page.
+ */
+
 public class RozetkaTests {
 
     @Test
-    public void shouldSearchForKeyword() throws InterruptedException {
+    public void shouldSearchForKeyword() {
         WebDriverManager.getInstance(ChromeDriver.class).setup();
         final WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://rozetka.com.ua");
         final WebElement element = driver.findElement(By.xpath("//input[@name=\"text\"]"));
         element.sendKeys("GeForce");
-        Thread.sleep(5000);
+        Try.run(() -> Thread.sleep(5000)).onFailure(System.out::println);
         driver.quit();
     }
 }
